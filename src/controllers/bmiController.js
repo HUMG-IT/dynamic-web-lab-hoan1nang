83c42ee60d2b400bb0c70ff8eec3920025fa527a
hoan1nang <hoan1nang@gmail.com>
2024-11-10 17:50:47 +0700

    Nâng cấp mã của ứng dụng này để cho phép Tính chỉ số BMI trực tuyến.

--- src/controllers/bmiController.js
+++ src/controllers/bmiController.js
@@ -1,8 +1,20 @@
-// Import các hàm calculateBMI và classifyBMI từ bmi.js
-
-// Hàm getBMI xử lý yêu cầu từ client
-// Trả về JSON chứa bmi và classification
-
-// Xuất hàm getBMI
-
-// Lưu ý: Tham khảo mã trong tệp nameController.js
+const calculateBMI = (height, weight) => {
+    const bmi = weight / (height * height);
+    let category = '';
+ 
+    if (bmi < 18.5) category = 'Gầy';
+    else if (bmi < 24.9) category = 'Bình thường';
+    else if (bmi < 29.9) category = 'Thừa cân';
+    else category = 'Béo phì';
+ 
+    return { bmi: bmi.toFixed(2), category };
+ };
+ 
+ const calculateBMIHandler = (req, res) => {
+    const { height, weight } = req.body;
+    const result = calculateBMI(height, weight);
+    res.json(result);
+ };
+ 
+ module.exports = { calculateBMIHandler };
+ 
